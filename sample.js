49f3b1dcb2b842a9891545f5bad9e0c308c81773
daishi_hmr <daishi.hmr@gmail.com>
2012-10-09 16:57:55 +0900

    modify sample

--- sample.js
+++ sample.js
@@ -1,3 +1,4 @@
+// BulletMLファイル
 var xmlFiles = [ "sample-assets/[1943]_rolling_fire.xml",
         "sample-assets/[G_DARIUS]_homing_laser.xml",
         "sample-assets/[Guwange]_round_2_boss_circle_fire.xml",
@@ -21,8 +22,7 @@ window.onload = function() {
     game.fps = 60;
     var assets = [ "sample-assets/chara0.png", "sample-assets/chara6.png",
             "sample-assets/explosion.png" ];
-    assets = assets.concat(xmlFiles);
-    game.preload(assets);
+    game.preload(assets.concat(xmlFiles));
     game.onload = function() {
         // var scene = game.rootScene;
         var scene = (function() {
@@ -47,16 +47,15 @@ window.onload = function() {
             }
 
             // 自機中心マーカーを移動させる
-            h.x = this.x + (this.width - h.width) / 2;
-            h.y = this.y + (this.height - h.height) / 2;
+            playerCenter.x = this.x + (this.width - playerCenter.width) / 2;
+            playerCenter.y = this.y + (this.height - playerCenter.height) / 2;
         });
         scene.addChild(player);
-
         // 自機の中心マーカー
-        var h = new Sprite(8, 8);
+        var playerCenter = new Sprite(8, 8);
         (function() {
-            h.image = new Surface(8, 8);
-            var c = h.image.context;
+            playerCenter.image = new Surface(8, 8);
+            var c = playerCenter.image.context;
             var g = c.createRadialGradient(4, 4, 0, 4, 4, 4);
             g.addColorStop(0.0, "#ffffff");
             g.addColorStop(0.5, "#aaffaa");
@@ -64,25 +63,7 @@ window.onload = function() {
             c.fillStyle = g;
             c.fillRect(0, 0, 8, 8);
         })();
-        scene.addChild(h);
-
-        // 爆発
-        var explode = function(x, y) {
-            var e = new Sprite(32, 32);
-            e.x = x - 16;
-            e.y = y - 16;
-            e.scale(2);
-            e.rotate(Math.random() * 360);
-            e.image = game.assets["sample-assets/explosion.png"];
-            e.on("enterframe", function() {
-                this.frame += 1;
-                if (64 <= this.frame) {
-                    this.removeEventListener("enterframe", arguments.callee);
-                    this.parentNode.removeChild(this);
-                }
-            });
-            scene.addChild(e);
-        };
+        scene.addChild(playerCenter);
 
         // 敵
         var enemy = new Sprite(32, 32);
@@ -100,46 +81,51 @@ window.onload = function() {
         scene.addChild(enemy);
 
         // 攻撃パターン
-        var test = BulletML
-                .build("<bulletml><action label='top'><fire><bullet/></fire></action></bulletml>");
-        var attackPattern = new AttackPattern(game.assets[xmlFiles[1]]);
+        var attackPattern = new AttackPattern(game.assets[xmlFiles[~~(Math
+                .random() * xmlFiles.length)]]);
+        // 攻撃パターン設定
+        var config = {
+            target : player, // 攻撃対象
+            bulletFactory : function(spec) { // 弾Spriteの生成関数
+                var bullet = new enchant.Sprite(8, 8);
+                bullet.image = enchant.Surface
+                        .load(enchant.bulletml.DEFAULT_IMAGE);
+                bullet.on("enterframe", function() {
+                    // 衝突判定（自機と弾との距離が4未満）
+                    var x1 = this.x + this.width / 2;
+                    var y1 = this.y + this.height / 2;
+                    var x2 = player.x + player.width / 2;
+                    var y2 = player.y + player.height / 2;
+                    var dx = (x1 - x2) * (x1 - x2);
+                    var dy = (y1 - y2) * (y1 - y2);
+                    if (dx + dy < 4 * 4) {
+                        this.parentNode.removeChild(this);
+                        explode(x1, y1);
+                    }
+                });
+                return bullet;
+            }
+        };
 
         // enterframeイベントリスナを作成
-        var ticker = attackPattern.createTicker({
-            target : player
-        });
+        var ticker = attackPattern.createTicker(config);
 
         // 作成したenterframeイベントリスナを敵機にセット
         enemy.on("enterframe", ticker);
 
-        // 攻撃完了イベント
+        // 攻撃完了時の処理
         enemy.on("completeAttack", function() {
-            this.removeEventListener("enterframe", ticker);
-            var xml = xmlFiles[~~(Math.random() * xmlFiles.length)];
-            console.log(xml);
-            ticker = new AttackPattern(game.assets[xml]).createTicker({
-                target : player
-            });
+            // 攻撃パターンさしかえ
+            this.removeEventListener(ticker);
+            attackPattern = new AttackPattern(game.assets[xmlFiles[~~(Math
+                    .random() * xmlFiles.length)]]);
+            ticker = attackPattern.createTicker(config);
             this.on("enterframe", ticker);
         });
 
-        // var histTest = function() {
-        // // 衝突判定（自機と弾との距離が4未満）
-        // var x1 = this.x + this.width / 2;
-        // var y1 = this.y + this.height / 2;
-        // var x2 = player.x + player.width / 2;
-        // var y2 = player.y + player.height / 2;
-        // var dx = (x1 - x2) * (x1 - x2);
-        // var dy = (y1 - y2) * (y1 - y2);
-        // if (dx + dy < 4 * 4) {
-        // this.parentNode.removeChild(this);
-        // explode(x1, y1);
-        // }
-        // };
-
         // タッチ操作用パネル
         var ctrlPanel = new Sprite(game.width, game.height);
-        ctrlPanel.sense = 1.4;
+        ctrlPanel.sense = 1.5;
         ctrlPanel.on("touchstart", function(e) {
             this.startX = e.x;
             this.startY = e.y;
@@ -165,7 +151,26 @@ window.onload = function() {
                 this.startY = e.y;
             }
         });
-        scene.addChild(ctrlPanel);
+        game.rootScene.addChild(ctrlPanel);
+
+        // 爆発
+        var explode = function(x, y) {
+            var e = new Sprite(32, 32);
+            e.x = x - 16;
+            e.y = y - 16;
+            e.scale(2);
+            e.rotate(Math.random() * 360);
+            e.image = game.assets["sample-assets/explosion.png"];
+            e.on("enterframe", function() {
+                this.frame += 1;
+                if (64 <= this.frame) {
+                    this.removeEventListener("enterframe", arguments.callee);
+                    this.parentNode.removeChild(this);
+                }
+            });
+            scene.addChild(e);
+        };
+
     };
     game.start();
 };
