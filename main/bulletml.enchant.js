024240899c7cf5ea16130a870fa77ffa7e412c38
daishi_hmr <daishi.hmr@gmail.com>
2012-10-11 05:05:10 +0900

    start develop v0.3.0

--- main/bulletml.enchant.js
+++ main/bulletml.enchant.js
@@ -1,7 +1,7 @@
 /*
- * bullet.enchant.js v0.2.2
+ * bullet.enchant.js v0.3.0
  * @author daishi@dev7.jp
- * @require enchant.js v0.5.1 or later, bulletml.js v0.1.0.
+ * @require enchant.js v0.5.1 or later, bulletml.js v0.3.0.
  * @description
  * enchant.js extension plugin for use BulletML.
  * 
