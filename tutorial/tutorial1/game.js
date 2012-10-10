8bec2569b70e01fc0bc9a4def979d0f5beb12714
daishi_hmr <daishi.hmr@gmail.com>
2012-10-11 04:12:18 +0900

    update tutorial1

--- tutorial/tutorial1/game.js
+++ tutorial/tutorial1/game.js
@@ -3,6 +3,7 @@ window.onload = function() {
     var game = new Game();
     game.preload("chara1.png");
     game.onload = function() {
+        // （自機）茶くま
         var player = new Sprite(32, 32);
         player.image = game.assets["chara1.png"];
         player.frame = 0;
@@ -10,6 +11,7 @@ window.onload = function() {
         player.y = 288;
         game.rootScene.addChild(player);
 
+        // （敵機）白くま
         var enemy = new Sprite(32, 32);
         enemy.image = game.assets["chara1.png"];
         enemy.frame = 5;
@@ -17,12 +19,14 @@ window.onload = function() {
         enemy.y = 0;
         game.rootScene.addChild(enemy);
 
-        var data = BulletML
-                .build("<bulletml><action label='top'><fire><bullet/></fire></action></bulletml>");
-        var pattern = new AttackPattern(data);
+        // BulletML
+        var bulletml = "<bulletml><action label='top'><fire><bullet/></fire></action></bulletml>";
+        // 攻撃パターンオブジェクト
+        var pattern = new AttackPattern(BulletML.build(bulletml));
+        // enterframeイベントリスナを生成
         var ticker = pattern.createTicker(player);
-        
-        enemy.on("enterframe", ticker);
+        // 生成したイベントリスナを敵機に登録
+        enemy.addEventListener("enterframe", ticker);
     };
     game.start();
 };
