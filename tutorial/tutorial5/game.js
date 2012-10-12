c1cecd5ecf7b4d22d942234e1c2dbacbbc30025e
daishihmr <daishi.hmr@gmail.com>
2012-10-12 18:33:33 +0900

    add tutorial5

---
+++ tutorial/tutorial5/game.js
@@ -0,0 +1,60 @@
+enchant();
+window.onload = function() {
+    var game = new Game();
+    game.preload("chara1.png", "icon0.png", "labeled.xml");
+    game.onload = function() {
+        var player = new Sprite(32, 32);
+        player.image = game.assets["chara1.png"];
+        player.frame = 0;
+        player.x = 144;
+        player.y = 288;
+        game.rootScene.addChild(player);
+        player.addEventListener("enterframe", function() {
+            if (game.input.left) {
+                this.x -= 5;
+            } else if (game.input.right) {
+                this.x += 5;
+            }
+        });
+
+        var enemy = new Sprite(32, 32);
+        enemy.image = game.assets["chara1.png"];
+        enemy.frame = 5;
+        enemy.x = 144;
+        enemy.y = 0;
+        game.rootScene.addChild(enemy);
+
+        var pattern = new AttackPattern(game.assets["labeled.xml"]);
+        var ticker = pattern.createTicker({
+            target : player,
+            bulletFactory : function(spec) {
+                var bullet = new Sprite(16, 16);
+                bullet.image = game.assets["icon0.png"];
+                switch (spec.label) {
+                case "spade":
+                    bullet.frame = 67;
+                    break;
+                case "clover":
+                    bullet.frame = 68;
+                    break;
+                case "diamond":
+                    bullet.frame = 69;
+                    break;
+                case "heart":
+                    bullet.frame = 70;
+                    break;
+                }
+                bullet.addEventListener("enterframe", function() {
+                    if (this.within(player, 10)) {
+                        alert("hit!");
+                        game.stop();
+                    }
+                });
+                return bullet;
+            }
+        });
+
+        enemy.on("enterframe", ticker);
+    };
+    game.start();
+};
