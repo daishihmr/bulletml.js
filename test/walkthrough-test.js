99ded05cc1813994470a05ad5e24901873d7ca2c
daishi_hmr <daishi.hmr@gmail.com>
2012-10-15 07:29:51 +0900

    thinking…

--- test/walkthrough-test.js
+++ test/walkthrough-test.js
@@ -5,10 +5,10 @@ var WtTest = TestCase("WalkthroughTest");
 WtTest.prototype.testBasic1 = function() {
     var bulletml = BulletML
             .build("<bulletml><action label='top'><fire><bullet/></fire></action></bulletml>");
-    var seq = bulletml.createWalker();
-    var fire = seq.next();
-    assertFalse(seq.next());
-    assertEquals("fire", fire.commandName);
+    var script = bulletml._scriptalize();
+    assertEquals("this.top=function(){" //
+            + "this.fire(null,null,this.bullet(this.direction(),this.speed(),null));" //
+            + "};");
 };
 
 WtTest.prototype.testBasic2 = function() {
@@ -16,12 +16,47 @@ WtTest.prototype.testBasic2 = function() {
             .build("<bulletml>"
                     + "<action label='top'><repeat><times>2</times><action><fire><bullet/></fire><vanish/></action></repeat></action>"
                     + "</bulletml>");
-    var walker = bulletml.createWalker();
-    assertEquals("fire", walker.next().commandName);
-    assertEquals("vanish", walker.next().commandName);
-    assertEquals("fire", walker.next().commandName);
-    assertEquals("vanish", walker.next().commandName);
-    assertFalse(walker.next());
+    var script = bulletml._scriptalize();
+    assertEquals("this.top=function(){" //
+            + "this.repeat(2,function(){" //
+            + "this.fire(null,null,this.bullet(this.direction(),this.speed(), null));" //
+            + "this.vanish();" //
+            + "});" //
+            + "};");
+};
+
+WtTest.prototype.testBulletRef = function() {
+    var bulletml = BulletML
+            .build("<bulletml><action label='top'><fire><bulletRef label='b1'/></fire></action>"
+                    + "<bullet label='b1'><action><wait>10</wait><vanish/></action></bullet></bulletml>");
+    var script = bulletml._scriptalize();
+    assertEquals("this.top=function(){" //
+            + "this.fire(null,null,this.b1());" //
+            + "};" //
+            + "this.b1=function($1,$2,$3,$4,$5){" //
+            + "return this.bullet(this.direction(),this.speed(),function(){" //
+            + "this.wait(10);" //
+            + "this.vanish();" //
+            + "});" //
+            + "};");
+};
+
+WtTest.prototype.testParameter = function() {
+    var bulletml = BulletML
+            .build("<bulletml>"
+                    + "<action label='top'><fire><bulletRef label='b1'><param>$rand</param><param>4</param></bulletRef></fire></action>"
+                    + "<bullet label='b1'><speed>$1</speed><action><wait>$2</wait><vanish/></action></bullet>"
+                    + "</bulletml>");
+    var script = bulletml._scriptalize();
+    assertEquals("this.top=function(){" //
+            + "this.fire(null,null,this.b1(Math.random(),4));" //
+            + "};" //
+            + "this.b1=function($1,$2,$3,$4,$5){" //
+            + "return this.bullet(this.direction(),this.speed($1),function(){" //
+            + "this.wait($2);" //
+            + "this.vanish();" //
+            + "});" //
+            + "};");
 };
 
 // ActionRef, FireRefはcall. BulletRefはget.
