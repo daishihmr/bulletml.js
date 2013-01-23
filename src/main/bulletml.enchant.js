3de40824632e05c90864ce24a7c565cc3ca08f85
daishi_hmr <daishi.hmr@gmail.com>
2013-01-24 06:52:55 +0900

    fix

--- src/main/bulletml.enchant.js
+++ src/main/bulletml.enchant.js
@@ -491,7 +491,7 @@ enchant.bulletml = enchant.bulletml || {};
                 break;
             case "sequence":
                 ticker.dirIncr = d;
-                ticker.dirFin = ticker.direction + ticker.dirIncr * t;
+                ticker.dirFin = ticker.direction + ticker.dirIncr * (t-1);
                 break;
             }
             ticker.chDirEnd = this.age + t;
